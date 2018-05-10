6319f7525a5de9c1a294649255f59c010cba4577
thiyagarajan <thiyagarajangg0@gmail.com>
2018-05-10 16:56:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-var name = "susan"
+var name = "Joe"
 var height = "74"
-if (name==="susan"){
-  console.log("the name is sussan")
-}
-else{
-  console.log("Expected the name to be susan")
-}
+
 // Don't worry about this
 module.exports = { name, height
 }
